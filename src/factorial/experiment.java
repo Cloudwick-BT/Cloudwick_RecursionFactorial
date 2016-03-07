4366bfb9f3650cd89ab01b475e270f4b788a52c1
Bhavin Tandel <Bhavin Tandel>
2016-03-07 10:00:15 +0000

    Checking stack overflow

---
+++ src/factorial/experiment.java
@@ -0,0 +1,44 @@
+package factorial;
+import java.util.*;
+/**
+ * Created by Bhavin on 07/03/2016.
+ */
+public class experiment {
+
+
+        public static int factMethod(int num){
+            if (num == 0){
+                return 1;
+            }
+            else{
+                return num+factMethod(num-1);
+            }
+        }
+
+        public static void main(String[] args){
+            int num=0;
+            Scanner inputScan = new Scanner(System.in);
+            System.out.println("Enter the number to be checked : ");
+            try{
+                num = inputScan.nextInt();
+                if (num < 0 ){
+                    System.out.print("Input need to be positive");
+                    inputScan.close();
+                    return;
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.print("Number must be an integer");
+                inputScan.close();
+                return;
+            }
+            finally{
+                inputScan.close();
+            }
+
+            System.out.print(factMethod(num));
+
+        }
+
+
+}
